f3ea10f70c703ae588c5fa4f603811c00e732405
Jeff Thompson <mail@jeffreythompson.org>
2017-10-16 09:36:32 -0400

    Now turns on in the right order

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -3,20 +3,21 @@
 #include <Wire.h>
 
 /*
-EVERY RADIO STATION: FIRMWARE
-Jeff Thompson | 2017 | jeffreythompson.org
+  EVERY RADIO STATION: FIRMWARE
+  Jeff Thompson | 2017 | jeffreythompson.org
 
-Control software for "Every Radio Station". This project was
-supported by a residency at Wave Farm.
+  Control software for "Every Radio Station". This project was
+  supported by a residency at Wave Farm.
 
-NOTE ON FREQUENCIES
-Installations in the Americas, Europe, Australia, Africa, and Asia
-(except Japan) can all be accounted for in code below. Installations
-in Japan, some of Eastern Europe, and Russia may need manual changes
-to this code in order to work, or may not be possible at all.
+  NOTE ON FREQUENCIES
+  Installations in the Americas, Europe, Australia, Africa, and Asia
+  (except Japan) can all be accounted for in code below. Installations
+  in Japan, some of Eastern Europe, and Russia may need manual changes
+  to this code in order to work, or may not be possible at all.
 
-HARDWARE
-Setup for 8 radios per section, 12 of these setups total*:
+  HARDWARE
+  Setup for 8 radios per section, 12 of these setups total (the final
+  section actually has only 7 radios, for a total of 95)
 
   + Custom-milled 3.3V and ground power bus PCB
   + Arduino Pro Mini 3.3V
@@ -24,28 +25,27 @@ Setup for 8 radios per section, 12 of these setups total*:
   + Si4973 FM radio breakout (Sparkfun)
   + Tymphany TC9FD18 speaker (Parts Express)
 
-  * the final section has only 7 radios, for a total of 95
-
 */
 
-const int whichSection =   1;       // which section of radios? (0–11)
+const int whichSection = 9;       // which section of radios? (0–11)
 
-const boolean debug =      true;    // debug over serial? (turn off for final install)
+const boolean debug =    true;    // debug over serial? (turn off for final install)
 
-const int SDIO =           A4;      // I2C pins on Arduino
-const int SCLK =           A5;
+const int SDIO =         A4;      // I2C pins on Arduino
+const int SCLK =         A5;
 
 // all the radios
 // first value is the digital pin that radio's reset
 // pin is connected to (must be unique for each radio)
+// backwards since I wired them wrong :(
 Si4703_Breakout radios[8] = {
-  Si4703_Breakout(2, SDIO, SCLK), 
-  Si4703_Breakout(3, SDIO, SCLK), 
-  Si4703_Breakout(4, SDIO, SCLK), 
-  Si4703_Breakout(5, SDIO, SCLK), 
-  Si4703_Breakout(6, SDIO, SCLK), 
-  Si4703_Breakout(7, SDIO, SCLK), 
-  Si4703_Breakout(8, SDIO, SCLK), 
+  Si4703_Breakout(2, SDIO, SCLK),
+  Si4703_Breakout(3, SDIO, SCLK),
+  Si4703_Breakout(4, SDIO, SCLK),
+  Si4703_Breakout(5, SDIO, SCLK),
+  Si4703_Breakout(6, SDIO, SCLK),
+  Si4703_Breakout(7, SDIO, SCLK),
+  Si4703_Breakout(8, SDIO, SCLK),
   Si4703_Breakout(9, SDIO, SCLK)
 };
 
@@ -53,7 +53,7 @@ Si4703_Breakout radios[8] = {
 // can be used to lessen the volume of really loud stations
 // values in adjustment array should be -15 – +15
 const boolean useVolumeAdjustments = false;
-const int[8] volumeAdjustments = {
+const int volumeAdjustments[8] = {
   0, 0, 0, 0, 0, 0, 0, 0
 };
 
@@ -70,22 +70,23 @@ const boolean tuneEvenStations = false;   // tune even-numbered stations (ex 88.
 
 const boolean delayStart = true;          // delay start so they all come on in one nice row?
                                           // super not-necessary, but fun
-const int startupDelay =   540;           // millis to delay for each set to begin
+const long startupDelay =  5600;          // millis to delay for each set to begin
 
 
 void setup() {
 
   if (debug) {
     Serial.begin(9600);
+    Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     Serial.println("EVERY RADIO STATION: INITIALIZING");
-    Serial.print("Section: ");
-    Serial.println(whichSection);    
+    Serial.print("Section:       ");
+    Serial.println(whichSection);
   }
 
   // find starting channel for this section
   // is a 3-digit integer value (991 = 99.1FM)
   // can also account for running up even-numbered stations (ie 88.8) if desired
-  int startChannel = 875 + ((whichSection) * 16);
+  int startChannel = 879 + ((whichSection) * 16);
   if (tuneEvenStations) startChannel = 876 + ((whichSection) * 16);
   if (debug) {
     Serial.print("Start channel: ");
@@ -93,10 +94,12 @@ void setup() {
   }
 
   // set # of radios, depending on which section we're in
-  int numRadios = 8;                        // most sections have 8 radios
-  if (whichSection == 11)  numRadios = 7;   // final section only has 7
+  int numRadios = 8;            // most sections have 8 radios
+  if (whichSection == 11)  {
+    numRadios = 7;              // final section only has 7
+  }
   if (debug) {
-    Serial.print("# radios: ");
+    Serial.print("# radios:      ");
     Serial.println(numRadios);
   }
 
@@ -108,13 +111,20 @@ void setup() {
   pinMode(medVolPin, INPUT_PULLUP);
   int volume = checkVolumeJumpers();
   if (debug) {
-    Serial.print("Volume: ");
+    Serial.print("Volume:        ");
     Serial.println(volume);
+    Serial.println("");
   }
 
   // delay starting, so they all come on in a nice long row
+  int startTime = millis();
   if (delayStart) {
-    delay(whichSection * startupDelay);
+    long delayTime = whichSection * startupDelay;
+    Serial.print("(Delaying startup ");
+    Serial.print(delayTime);
+    Serial.println(" ms...)");
+    delay(delayTime);
+    Serial.println("");
   }
 
   // start up all the radios in this section
@@ -124,22 +134,22 @@ void setup() {
       Serial.println("(using individual volume adjustments)");
     }
   }
-  for (int i=0; i<numRadios; i++) {
+  for (int i=numRadios-1; i>=0; i-=1) {       // backwards, since I wired them wrong :(
     selectOutput(i);                          // select I2C output channel
     radios[i].powerOn();                      // turn it on
-    int channel = startChannel + (i*2);       // FM moves by 2, after 99.5 is 99.7, etc
-    radios[i].setChannel(channel);
+    int channel = startChannel + (i * 2);     // FM moves by 2, after 99.5 is 99.7, etc
     if (debug) {
       Serial.print("- freq: ");
       Serial.print(channel);
     }
+    radios[i].setChannel(channel);
     
     // set volume, either to value set by jumper
     // or adjusted individually
     int vol = volume;
     if (useVolumeAdjustments) {
       vol += volumeAdjustments[i];
-      vol = constrain(vol, 0,15);
+      vol = constrain(vol, 0, 15);
     }
     radios[i].setVolume(vol);
     if (debug) {
@@ -149,12 +159,18 @@ void setup() {
   }
 
   // that's it
-  if (debug) Serial.println("\nALL READY!");
+  if (debug) {
+    Serial.println("\nALL READY!");
+    Serial.print("(startup time = ");
+    Serial.print(millis() - startTime);
+    Serial.println(")");
+
+  }
 }
 
 
 void loop() {
-  // nothin'
+  // nothing here
 }
 
 
@@ -176,7 +192,7 @@ void selectOutput(uint8_t i) {
   if (i > 7) return;
   Wire.beginTransmission(0x70);
   Wire.write(1 << i);
-  Wire.endTransmission();  
+  Wire.endTransmission();
 }
 
 
